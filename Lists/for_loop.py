1950063fb5ef839650c8c37dedce42a4a82d53fa
Tyler Beck <tbeck202@gmail.com>
2020-07-15 19:38:33 -0700

    working with lists

---
+++ Lists/for_loop.py
@@ -0,0 +1,21 @@
+colors = ['purple', 'teal', 'magenta', 'crimson', 'clover']
+
+# for color in colors:
+#     print(color)
+
+numbers = [4, 3, 5, 7, 8, 54, 2]
+
+# for num in numbers:
+#     print(num * num)
+
+i = 0
+while i < len(colors):
+    print(colors[i])
+    i += 1
+
+words = ['super', 'calif', 'raga', 'listic', 'expeal', 'idocious']
+
+word = ''
+for w in words:
+    word += w.upper()
+print(word)
