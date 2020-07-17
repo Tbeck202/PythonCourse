ac03bece77a8eec858198ee5bfef5de7a697cefe
Tyler Beck <tbeck202@gmail.com>
2020-07-16 19:13:05 -0700

    lernin' slice

---
+++ Lists/slice.py
@@ -0,0 +1,8 @@
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+colors2 = colors[1:3]
+print(colors2)
+colors3 = colors[:1:-1]
+print(colors3)
+colors4 = colors[1::2]
+print(colors4)
