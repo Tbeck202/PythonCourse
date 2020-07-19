5ff7c85aae35a36dc8b0aeca4bc189f1f50ac788
Tyler Beck <tbeck202@gmail.com>
2020-07-19 14:13:48 -0700

    functions

---
+++ Functions/basic_functions.py
@@ -0,0 +1,19 @@
+# def sing_happy_bday():
+#     print("Happy birthday to you")
+#     print("Happy birthday to you")
+#     print("Happy birthday dear you")
+#     print("Happy birthday to you")
+
+
+# sing_happy_bday()
+
+# =============================
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def print_square(num):
+    return num**2
+
+
+for num in nums:
+    print(print_square(num))
