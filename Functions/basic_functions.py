3ad05e83b2377cbd37a5b7a928f40409fee4b3e0
Tyler Beck <tbeck202@gmail.com>
2020-07-27 07:43:49 -0700

    Update basic_functions.py

--- Functions/basic_functions.py
+++ Functions/basic_functions.py
@@ -8,12 +8,32 @@
 # sing_happy_bday()
 
 # =============================
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def print_square(num):
-    return num**2
+# def square(num):
+#     return num**2
 
 
-for num in nums:
-    print(print_square(num))
+# for num in nums:
+#     print(square(num))
+
+
+# =============================
+
+# def add(a, b):
+#     return a + b
+
+
+# print(add(5, 3))
+
+# print(square(add(5, 3)))
+
+# =============================
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+print(divide(2, 5))
+print(divide(5, 2))
